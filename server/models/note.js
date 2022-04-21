62d182f3e230f6320bfea876aee9a4ca914c9b5e
RaymondLee552 <raymond.leemmxix@gmail.com>
2022-04-21 01:30:30 -0400

    remove dummy code, add notes object sample

---
+++ server/models/note.js
@@ -0,0 +1,14 @@
+const notes = [
+    {
+      noteId: 12345,
+      content: "hello",
+    },
+    {
+        noteId: 55555,
+        content: "fredburger54",
+    },
+    {
+        noteId: 34212,
+        content: "coolcathy34",
+    }
+  ]
