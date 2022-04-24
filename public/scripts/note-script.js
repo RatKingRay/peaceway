8d89d16652c0d3b7acfe8ae4ee8c88146fb493ae
RaymondLee552 <raymond.leemmxix@gmail.com>
2022-04-24 15:58:04 -0400

    Update note-script.js
    Changed how we create the actual note html

--- public/scripts/note-script.js
+++ public/scripts/note-script.js
@@ -3,40 +3,105 @@ import
 { fetchData } 
 from './main.js'
 
-// function setNote(note) {
-//   localStorage.setItem('user', JSON.stringify(user));
-// }
-
-// function getNotes(e) {
-//   e.preventDefault()
-
-//   return JSON.parse(localStorage.getItem('note'));
-// }
-
-// function retrieve() {
-//   //Retrieve other stored notes and put them up here
-
-//   // fetchData('/users/retrieve', {content: contentTemp, mood: moodTemp, is_vent: is_ventTemp}, "GET")
-//   // .then((data) => {
-//   //   if(!data.message) {
-//   //     //displayNote(data)
-//   //     data.forEach(note => console.log(note))
-//   //     data.forEach(note => displayNote(note))
-//   //     // window.location.href = "notes.html";
-//   //   }
-//   // })
-//   // .catch((error) => {
-//   //   const errText = error.message;
-//   //   // document.querySelector("#loginForm p.error").innerHTML = errText;
-//   //   // document.getElementById("pswd").value = "";
-//   //   console.log(`Error! ${errText}`)
-//   // })
-// }
-
 //Have to make code to wipe is_vent notes
 
 let noteForm = document.getElementById("noteForm")
 if(noteForm) noteForm.addEventListener('submit', addNote)
+
+function addNote(e) {
+    e.preventDefault()
+
+    const flexNotes = document.getElementById("flex-notes")
+    const contentTemp = document.getElementById("note").value
+    const moodTemp = document.getElementById("emotion").value
+    const is_ventTemp = document.getElementById("is_vent").value
+
+    fetchData('/notes/create', {content: contentTemp, mood: moodTemp, is_vent: is_ventTemp}, "POST")
+    .then((data) => {
+      if(!data.message) {
+        console.log(data)
+        localStorage.setItem('note', JSON.stringify(data))
+        console.log("Post success")
+      }
+    })
+    .catch((error) => {
+      const errText = error.message;
+      console.log(`Error! ${errText}`)
+    })
+
+    let p = document.createElement('p')
+    p.className = "note"
+    p.innerHTML = `
+      ${contentTemp}
+      <hr>
+      <br>
+      This made you feel: ${moodTemp}
+      <br>
+      <button class="button" id="deleteBtn">Delete</button>
+    `
+    p.addEventListener('click', deleteNote())
+
+    // const newNote = new Note(note)
+    // console.log(newNote)
+
+    flexNotes.appendChild(p)
+
+    document.getElementById("note").value = ""
+}
+
+
+//Btn is only local so we have a bit of an issue
+//If we made it so we just retireved from database than we can just drop it from
+//db and refresh the page
+// let deleteBtn = document.getElementById("deleteBtn")
+// if(deleteBtn) deleteBtn.addEventListener('click', deleteNote) 
+
+function deleteNote() { 
+  console.log("Hello?")
+  
+  fetchData('/users/delete_note', {noteId: note.noteId}, "DELETE")
+  .then((data) => {
+    if(!data.message) {
+      console.log(data.success)
+      window.location.href = "notes.html"
+    }
+  })
+  .catch((error) => {
+    const errText = error.message 
+    // document.querySelector("#profile div p.error").innerHTML = errText 
+    console.log(`Error! ${errText}`)
+  })
+}
+
+// function displayNote(data) {
+
+//     const flexNotes = data[0]
+//     const contentTemp = document.getElementById("note").value
+//     const moodTemp = document.getElementById("emotion").value
+//     const is_ventTemp = document.getElementById("is_vent").value
+
+//     //const newNote = new Note(note)
+//     //console.log(newNote)
+//     //return JSON.parse(localStorage.getItem('user'));
+
+//     let p = document.createElement('p')
+//     p.appendChild(document.createTextNode(contentTemp))
+//     p.appendChild(document.createElement('hr'))
+//     p.appendChild(document.createElement('br'))
+//     p.appendChild(document.createTextNode(`This made you feel: ${moodTemp}`))
+//     p.appendChild(document.createElement('br'))
+//     p.className = "note"
+//     flexNotes.appendChild(p)
+
+//     let button = document.createElement('button')
+//     button.appendChild(document.createTextNode("Delete"))
+//     button.id = "deleteBtn"
+//     button.className = "button"
+//     p.appendChild(button)
+
+
+//     document.getElementById("note").value = ""
+// }
 
 // function addNote(e) {
 //     e.preventDefault()
@@ -81,99 +146,32 @@ if(noteForm) noteForm.addEventListener('submit', addNote)
 //     // document.getElementById("note").value = ""
 // }
 
-//Working addNote, but doesnt work with database
-function addNote(e) {
-    e.preventDefault()
+// function setNote(note) {
+//   localStorage.setItem('user', JSON.stringify(user));
+// }
 
-    const flexNotes = document.getElementById("flex-notes")
-    const contentTemp = document.getElementById("note").value
-    const moodTemp = document.getElementById("emotion").value
-    const is_ventTemp = document.getElementById("is_vent").value
+// function getNotes(e) {
+//   e.preventDefault()
 
-    fetchData('/notes/create', {content: contentTemp, mood: moodTemp, is_vent: is_ventTemp}, "POST")
-    .then((data) => {
-      if(!data.message) {
-        console.log(data)
-        localStorage.setItem('note', JSON.stringify(data))
-        console.log("Post success")
-      }
-    })
-    .catch((error) => {
-      const errText = error.message;
-      console.log(`Error! ${errText}`)
-    })
+//   return JSON.parse(localStorage.getItem('note'));
+// }
 
-    // const newNote = new Note(note)
-    // console.log(newNote)
+// function retrieve() {
+//   //Retrieve other stored notes and put them up here
 
-    let p = document.createElement('p')
-    p.appendChild(document.createTextNode(contentTemp))
-    p.appendChild(document.createElement('hr'))
-    p.appendChild(document.createElement('br'))
-    p.appendChild(document.createTextNode(`This made you feel: ${moodTemp}`))
-    p.appendChild(document.createElement('br'))
-    p.className = "note"
-    flexNotes.appendChild(p)
-
-    let button = document.createElement('button')
-    button.appendChild(document.createTextNode("Delete"))
-    button.id = "deleteBtn"
-    button.className = "button"
-    p.appendChild(button)
-
-
-    document.getElementById("note").value = ""
-}
-
-function displayNote(data) {
-
-    const flexNotes = data[0]
-    const contentTemp = document.getElementById("note").value
-    const moodTemp = document.getElementById("emotion").value
-    const is_ventTemp = document.getElementById("is_vent").value
-
-    //const newNote = new Note(note)
-    //console.log(newNote)
-    //return JSON.parse(localStorage.getItem('user'));
-
-    let p = document.createElement('p')
-    p.appendChild(document.createTextNode(contentTemp))
-    p.appendChild(document.createElement('hr'))
-    p.appendChild(document.createElement('br'))
-    p.appendChild(document.createTextNode(`This made you feel: ${moodTemp}`))
-    p.appendChild(document.createElement('br'))
-    p.className = "note"
-    flexNotes.appendChild(p)
-
-    let button = document.createElement('button')
-    button.appendChild(document.createTextNode("Delete"))
-    button.id = "deleteBtn"
-    button.className = "button"
-    p.appendChild(button)
-
-
-    document.getElementById("note").value = ""
-}
-
-//Btn is only local so we have a bit of an issue
-//If we made it so we just retireved from database than we can just drop it from
-//db and refresh the page
-let deleteBtn = document.getElementById("deleteBtn")
-if(deleteBtn) deleteBtn.addEventListener('click', deleteNote) 
-
-function deleteNote() { 
-  console.log("Hello?")
-  
-  fetchData('/users/delete_note', {noteId: note.noteId}, "DELETE")
-  .then((data) => {
-    if(!data.message) {
-      console.log(data.success)
-      window.location.href = "notes.html"
-    }
-  })
-  .catch((error) => {
-    const errText = error.message 
-    // document.querySelector("#profile div p.error").innerHTML = errText 
-    console.log(`Error! ${errText}`)
-  })
-}
+//   // fetchData('/users/retrieve', {content: contentTemp, mood: moodTemp, is_vent: is_ventTemp}, "GET")
+//   // .then((data) => {
+//   //   if(!data.message) {
+//   //     //displayNote(data)
+//   //     data.forEach(note => console.log(note))
+//   //     data.forEach(note => displayNote(note))
+//   //     // window.location.href = "notes.html";
+//   //   }
+//   // })
+//   // .catch((error) => {
+//   //   const errText = error.message;
+//   //   // document.querySelector("#loginForm p.error").innerHTML = errText;
+//   //   // document.getElementById("pswd").value = "";
+//   //   console.log(`Error! ${errText}`)
+//   // })
+// }
